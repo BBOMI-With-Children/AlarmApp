683575ab895a25621e68c43e697fa0152a309765
가현 <102145605+rkgus24@users.noreply.github.com>
2025-08-05 00:13:12 +0900

    Initial Commit

---
+++ AlarmTests/AlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmTests.swift
+//  AlarmTests
+//
+//  Created by 노가현 on 8/5/25.
+//
+
+import Testing
+@testable import Alarm
+
+struct AlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
