b875a27409862714e667b4801bb70703b0ae3212
Gwangyong <key4168@icloud.com>
2025-08-07 01:43:10 +0900

    feat: #28 - 세계 시계 네비게이션 바 구성
    - 네비게이션 바에 좌측 '편집', 우측 '+' 버튼 추가
    - 편집 버튼 클릭 시 "편집" ↔ "완료" 토글 동작 구현
    - 네비게이션 타이틀 '세계 시계' 설정

--- Alarm/Scenes/World/WorldViewController.swift
+++ Alarm/Scenes/World/WorldViewController.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
 
 final class WorldViewController: UIViewController {
+  
+  private var isEditingMode = false
+  
   // MARK: - Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+    configureNavigationBar()
   }
 
   // MARK: - Private Methods
@@ -20,5 +28,33 @@ final class WorldViewController: UIViewController {
   private func configureUI() {
     view.backgroundColor = .systemBackground
     title = "세계 시계"
+  }
+  
+  private func configureNavigationBar() {
+    title = "세계 시계"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(
+      title: "편집",
+      style: .plain,
+      target: self,
+      action: #selector(didTapEditButton)
+    )
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .add,
+      target: self,
+      action: #selector(didTapAddButton)
+      )
+  }
+  
+  // MARK: - Actions
+  
+  @objc private func didTapEditButton() {
+    isEditingMode.toggle()
+    
+    navigationItem.leftBarButtonItem?.title = isEditingMode ? "완료" : "편집"
+  }
+  
+  @objc private func didTapAddButton() {
+    print("Tap")
   }
 }
