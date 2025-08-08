46f984abc9177998bc5436e68eaa5774314d5756
가현 <102145605+rkgus24@users.noreply.github.com>
2025-08-08 14:21:31 +0900

    feat: #22 - AlarmViewController에 AlarmManager 데이터 표시 기능 추가

--- Alarm/Scenes/Alarm/AlarmViewController.swift
+++ Alarm/Scenes/Alarm/AlarmViewController.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 final class AlarmViewController: UIViewController {
+  private let tableView = UITableView()
+
   // MARK: - Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureUI()
+    view.backgroundColor = .white
+
+    AlarmManager.shared.loadSampleData()
+
+    tableView.frame = view.bounds
+    tableView.dataSource = self
+    view.addSubview(tableView)
+  }
+}
+
+// MARK: - UITableViewDataSource
+
+extension AlarmViewController: UITableViewDataSource {
+  // 섹션별 행의 개수
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    AlarmManager.shared.alarms.count // 저장된 알람 개수
   }
 
-  // MARK: - Private Methods
-
-  private func configureUI() {
-    view.backgroundColor = .systemBackground
-    title = "알람"
+  // 행에 표시할 셀
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell() // 셀 생성
+    let alarm = AlarmManager.shared.alarms[indexPath.row] // 해당 위치의 알람 데이터
+    cell.textLabel?.text = "\(alarm.time) - \(alarm.subtitle)" // 셀에 알람 시간, 부제목 표시
+    return cell
   }
 }
