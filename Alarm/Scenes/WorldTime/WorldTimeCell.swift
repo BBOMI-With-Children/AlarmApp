f2f6df8505629ed17d021c76710081e97d84373d
Gwangyong <key4168@icloud.com>
2025-08-09 21:54:22 +0900

    add: #48 - WorldTimeCell 파일 생성
    - Cell 파일 초기 세팅

--- Alarm/Scenes/WorldTime/WorldTimeCell.swift
+++ Alarm/Scenes/WorldTime/WorldTimeCell.swift
@@ -5,3 +5,18 @@
 //  Created by 서광용 on 8/7/25.
 //
 
+import UIKit
+import SnapKit
+import Then
+
+final class WorldTimeCell: UITableViewCell {
+ static let id = "WorldTimeCell"
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
