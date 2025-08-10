359d81943aa46619c3c23d87d6ad76e2c54881f9
Gwangyong <key4168@icloud.com>
2025-08-10 22:23:18 +0900

    feat: #54 - 서치바 UI 스타일 구성
    - 서치바 추가 및 배경, 텍스트,placeholder 등 색상 설정
    - 커서 색상과 취소 버튼을 mainColor로 설정
    - 제목 라벨을 아이폰 기본 앱처럼 작은 크기로 설정

--- Alarm/Scenes/WorldTime/WorldTimeCitySelectionViewController.swift
+++ Alarm/Scenes/WorldTime/WorldTimeCitySelectionViewController.swift
@@ -10,18 +10,66 @@ import Then
 import UIKit
 
 final class WorldTimeCitySelectionViewController: UIViewController {
+  // MARK: - Properties
+  
+  private let backgroundColor = UIColor(named: "backgroundColor")
+  private let mainColor = UIColor(named: "mainColor")
+  
+  private let titleLabel = UILabel().then {
+    $0.text = "도시 선택"
+    $0.textColor = .white
+    $0.font = .systemFont(ofSize: 14, weight: .regular)
+    $0.textAlignment = .center
+  }
+  
+  private lazy var searchBar = UISearchBar().then {
+    $0.searchBarStyle = .default
+    $0.barTintColor = backgroundColor
+    $0.searchTextField.backgroundColor = UIColor.darkGray
+    $0.searchTextField.textColor = UIColor.white
+    $0.searchTextField.tintColor = mainColor // 커서 색 변경
+    $0.searchTextField.leftView?.tintColor = .lightGray // 돋보기 아이콘 색 변경
+    $0.showsCancelButton = true // searchBar에 기본으로 있는 cancel 버튼 사용
+    
+    // placeholder 색 변경
+    $0.searchTextField.attributedPlaceholder = NSAttributedString(
+      string: "검색",
+      attributes: [.foregroundColor: UIColor.lightGray]
+    )
+  }
+  
   // MARK: - Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .blue
+    setupUI()
+    setupLayout()
   }
   
   // MARK: - setupUI
 
-  private func setupUI() {}
+  private func setupUI() {
+    view.backgroundColor = backgroundColor
+    [titleLabel, searchBar].forEach { view.addSubview($0) }
+    
+    // searchBar에 있는 기본 cancel 버튼을 KVC 방식으로 찾아와서 커스텀
+    if let cancelButton = searchBar.value(forKey: "cancelButton") as? UIButton {
+      cancelButton.setTitle("취소", for: .normal)
+      cancelButton.setTitleColor(mainColor, for: .normal)
+    }
+  }
 
   // MARK: - setupLayout
 
-  private func setupLayout() {}
+  private func setupLayout() {
+    titleLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide).offset(4)
+      $0.centerX.equalToSuperview()
+    }
+    
+    searchBar.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+      $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(12)
+    }
+  }
 }
